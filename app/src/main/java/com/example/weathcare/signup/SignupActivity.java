93c46b5e856a19c024522eb9e50f51d641f2ba05
rsl081 <53944109+rsl081@users.noreply.github.com>
2021-07-15 18:21:11 +0800

    Signup Update Error

--- app/src/main/java/com/example/weathcare/signup/SignupActivity.java
+++ app/src/main/java/com/example/weathcare/signup/SignupActivity.java
@@ -10,7 +10,9 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Debug;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +21,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.weathcare.R;
+import com.example.weathcare.common.NodeNames;
 import com.example.weathcare.login.LoginActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -27,9 +30,14 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+
+
+import java.util.HashMap;
 
 public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -39,6 +47,7 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
     private Button btnSignup;
 
     private FirebaseUser firebaseUser;
+    private DatabaseReference databaseReference;
 
     private StorageReference fileStorage;
     private Uri localFileUri, serverFileUri;
@@ -63,71 +72,6 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
 
         fileStorage = FirebaseStorage.getInstance().getReference();
     }
-
-
-    @Override
-    public void onClick(View view) {
-        name = etName.getText().toString().trim();
-        email = etEmail.getText().toString().trim();
-        password = etPassword.getText().toString().trim();
-        confirmpassord = etConfirmPassword.getText().toString().trim();
-
-        switch (view.getId())
-        {
-            case R.id.imageViewSignup:
-                if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED)
-                {
-                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    startActivityForResult(intent, 101);
-                    Toast.makeText(this, "Hello Picurre!!", Toast.LENGTH_SHORT).show();
-                }else{
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},102);
-                }
-            break;
-            case R.id.signupBtn:
-                if(name.equals(""))
-                {
-                    etName.setError("Name is empty");
-                }else if(email.equals(""))
-                {
-                    etEmail.setError("Email is empty");
-                }else if(password.equals(""))
-                {
-                    etPassword.setError("Password is empty");
-                }else if(confirmpassord.equals(""))
-                {
-                    etConfirmPassword.setError("Confirm password is empty");
-                }else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
-                {
-                    etEmail.setError("Enter correct email");
-                }else if(!password.equals(confirmpassord))
-                {
-                    etConfirmPassword.setError("Password mismatch");
-                }else{
-                    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if(task.isSuccessful())
-                            {
-                                firebaseUser = firebaseAuth.getCurrentUser();
-
-                                if(localFileUri != null)
-                                {
-                                    UpdateNameAndPhoto();
-                                }else{
-                                    Toast.makeText(SignupActivity.this, "User created successfully", Toast.LENGTH_SHORT).show();
-                                    startActivity(new Intent(SignupActivity.this, LoginActivity.class));
-                                }
-                            } else{
-                                Toast.makeText(SignupActivity.this, "Signup Failed : " + task.getException(), Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-                }
-            break;
-        }
-    }//End of OnClick
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -171,12 +115,37 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
                     fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
                         @Override
                         public void onSuccess(Uri uri) {
-                             serverFileUri = uri;
+                            serverFileUri = uri;
 
                             UserProfileChangeRequest request = new UserProfileChangeRequest.Builder()
                                     .setDisplayName(etName.getText().toString().trim())
                                     .setPhotoUri(serverFileUri)
                                     .build();
+
+                            firebaseUser.updateProfile(request).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful())
+                                    {
+                                        String userID = firebaseUser.getUid();
+                                        databaseReference = FirebaseDatabase.getInstance().getReference().child(NodeNames.USERS);
+
+                                        HashMap<String, String> hashMap = new HashMap<>();
+
+                                        hashMap.put(NodeNames.NAME, etName.getText().toString().trim());
+                                        hashMap.put(NodeNames.EMAIL, etEmail.getText().toString().trim());
+                                        hashMap.put(NodeNames.PHOTO, serverFileUri.getPath());
+
+                                        databaseReference.child(userID).setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                Toast.makeText(SignupActivity.this, "User created successfully", Toast.LENGTH_SHORT).show();
+                                                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                                            }
+                                        });
+                                    }
+                                }
+                            });
                         }
                     });
                 }
@@ -185,6 +154,67 @@ public class SignupActivity extends AppCompatActivity implements View.OnClickLis
     }//End of UpdateNameAndPhoto
 
 
+    @Override
+    public void onClick(View view) {
+        name = etName.getText().toString().trim();
+        email = etEmail.getText().toString().trim();
+        password = etPassword.getText().toString().trim();
+        confirmpassord = etConfirmPassword.getText().toString().trim();
 
+        switch (view.getId())
+        {
+            case R.id.imageViewSignup:
+                if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)==PackageManager.PERMISSION_GRANTED)
+                {
+                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 101);
+                    Toast.makeText(this, "Hello Picurre!!", Toast.LENGTH_SHORT).show();
+                }else{
+                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},102);
+                }
+                break;
+            case R.id.signupBtn:
+                if(name.equals(""))
+                {
+                    etName.setError("Name is empty");
+                }else if(email.equals(""))
+                {
+                    etEmail.setError("Email is empty");
+                }else if(password.equals(""))
+                {
+                    etPassword.setError("Password is empty");
+                }else if(confirmpassord.equals(""))
+                {
+                    etConfirmPassword.setError("Confirm password is empty");
+                }else if(!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+                {
+                    etEmail.setError("Enter correct email");
+                }else if(!password.equals(confirmpassord))
+                {
+                    etConfirmPassword.setError("Password mismatch");
+                }else{
+                    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                            {
+                                firebaseUser = firebaseAuth.getCurrentUser();
 
+                                if(localFileUri != null)
+                                {
+                                    UpdateNameAndPhoto();
+                                }else{
+                                    Toast.makeText(SignupActivity.this, "User created successfully", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                                }
+                            } else{
+                                Toast.makeText(SignupActivity.this, "Signup Failed : " + task.getException(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+                break;
+        }
+    }//End of OnClick
 }
