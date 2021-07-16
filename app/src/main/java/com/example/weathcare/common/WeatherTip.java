d9219aa3a6a999a93e3ecb6ec98d0824f9cf072e
kurtf <kurtfelipe4@gmail.com>
2021-07-17 04:18:21 +0800

    home and other weather

--- app/src/main/java/com/example/weathcare/common/WeatherTip.java
+++ app/src/main/java/com/example/weathcare/common/WeatherTip.java
@@ -3,8 +3,12 @@ package com.example.weathcare.common;
 public class WeatherTip
 {
     //Hot
-    public static final String STAYHyrady = "happy";
+    public static final String stayHydrated = "Drink Plenty of Fluids regardless of how active you are.";
+    public static final String stayCool = "Wear Appropriate Clothes: Choose light weight, light colored, loose fitting clothing.";
+    public static final String stayInformed = "Check for Updates: Check your local news for extreme heat alerts.";
+    //rainy
+    public static final String warmAndDry = "Always pack an umbrella and a jacket or raincoat when going out. If forecast is bad, avoid going out. Safety first!";
+    public static final String chargePhone = "Communication is vital during emergencies. Your phone will be your best friend as you read updates and check family and friends.";
 
-    //Hot
 
 }
