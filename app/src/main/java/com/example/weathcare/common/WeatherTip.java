3a55dce20b3b33290eb83f6bf2eee3080eabb534
rsl081 <53944109+rsl081@users.noreply.github.com>
2021-07-17 13:09:31 +0800

    complete weathertip

--- app/src/main/java/com/example/weathcare/common/WeatherTip.java
+++ app/src/main/java/com/example/weathcare/common/WeatherTip.java
@@ -9,5 +9,13 @@ public class WeatherTip
     //rainy
     public static final String warmAndDry = "Always pack an umbrella and a jacket or raincoat when going out. If forecast is bad, avoid going out. Safety first!";
     public static final String chargePhone = "Communication is vital during emergencies. Your phone will be your best friend as you read updates and check family and friends.";
-
+    public static final String tuneToNews = "Monitor news reports and weather forecasts regularly. At the same time, keep your eyes and ears out for local government advice.";
+    //cold
+    public static final String dressWarmly = "Dress in layer, when it is cold, wear a hat, mittens or insulated gloves. Keep your face warm with a scarf.";
+    public static final String listenWeatherForecast = "Listen for a wind chill warning. Warning are based on local climate and are issued when significant wind chills are expected";
+    public static final String seekShelter = "When the wind chill is significant, get out of the wind and limit the time you spend outside";
+    //goodWeather
+    public static final String spendTime = "Set up date nights and fun nights! The days are longer, so it's a great time to really go out and have fun!";
+    public static final String startFitness = "Go hiking, riding, etc. Do something new fun! Make it better by bringing a friend.";
+    public static final String spendOutdoors = "Take some timeto spend outdoors. When we spend our time in nature, it naturally calms and gives us a feeling of inner peace.";
 }
